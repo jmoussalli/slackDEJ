8dea22615da05c797ffc760077f4eac7f088b0ba
jmoussalli <jerome.moussalli@gmail.com>
2024-02-06 14:10:41 +0100

    Channel entity test suite...

--- src/test/java/fr/moussalli/slackdej/entity/ChannelTest.java
+++ src/test/java/fr/moussalli/slackdej/entity/ChannelTest.java
@@ -1,23 +1,57 @@
 package fr.moussalli.slackdej.entity;
 
+import fr.moussalli.slackdej.repository.ChannelRepository;
+import fr.moussalli.slackdej.repository.PostRepository;
+import fr.moussalli.slackdej.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ChannelTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ChannelRepository channelRepository;
+
+
+    @BeforeAll
+    public static void loadEnvVariables() throws IOException {
+        Path envPath = Paths.get(".env");
+        Stream<String> lines = Files.lines(envPath);
+        lines.forEach(line -> {
+            String[] parts = line.split("=", 2);
+            if (parts.length == 2) {
+                System.setProperty(parts[0], parts[1]);
+            }
+        });
+    }
 
     @Test
     void getName() {
         Channel channel = new Channel();
         channel.setName("test");
-        assertEquals("test", channel.getName());
+        Channel channelSaved = channelRepository.save(channel);
+        assertEquals("test", channelSaved.getName());
     }
 
     @Test
